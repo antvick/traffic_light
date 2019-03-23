06190c25c357d6586a3d54492fddabaf03927d44
Ant Vickers <antvick@gmail.com>
2019-03-23 09:28:21 +0000

    Add GPIO control

--- src/api/blueprints/light.py
+++ src/api/blueprints/light.py
@@ -1,15 +1,18 @@
-from flask import Blueprint, request
+import RPi.GPIO as GPIO
+from flask import Blueprint, request, jsonify
 from typing import Dict
+from collections import namedtuple
 light_blueprint = Blueprint('light', __name__)
 
-gpiolightmap = {
-    "green": "1",
-    "yellow": "3", 
-    "red": "5"
-}
+GPIO.setmode(GPIO.BCM)
+GPIO.setwarnings(False)
+lamp = namedtuple('lamp', 'gpio colour')
+green = lamp(gpio=2, colour='green')
+amber = lamp(gpio=3, colour='amber')
+red = lamp(gpio=4, colour='red')
+lamps = [green, amber, red]
 
-@light_blueprint.route("/flash")
-def flash() -> None:
-    colour = request.args.get('colour')
-    return gpiolightmap[colour]
+@light_blueprint.route("/status")
+def status() -> None:
+    return jsonify({lamp.colour:GPIO.input(lamp.gpio) for lamp in lamps})
 
--- src/api/blueprints/test.py
+++ src/api/blueprints/test.py
@@ -1,6 +1,6 @@
 from flask import Blueprint
 test_blueprint = Blueprint('test', __name__)
-@test_blueprint.route("/status")
-def status():
+@test_blueprint.route("/test")
+def test():
     return "OK"
 
