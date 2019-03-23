85560367fdbeb791ff87864be7b4880bf7a3fbaf
Anthony Vickers <antvick@gmail.com>
2019-03-23 11:50:55 +0000

    Add api end-point to set state of lamps

--- src/api/blueprints/light.py
+++ src/api/blueprints/light.py
@@ -1,5 +1,5 @@
 import RPi.GPIO as GPIO
-from flask import Blueprint, request, jsonify
+from flask import Blueprint, request, jsonify, make_response
 from typing import Dict
 from collections import namedtuple
 light_blueprint = Blueprint('light', __name__)
@@ -10,11 +10,46 @@ lamp = namedtuple('lamp', 'gpio colour')
 green = lamp(gpio=2, colour='green')
 amber = lamp(gpio=3, colour='amber')
 red = lamp(gpio=4, colour='red')
+spare = lamp(gpio=17, colour=None)
 lamps = [green, amber, red]
+GPIO.setup(spare.gpio, GPIO.OUT)
+GPIO.output(spare.gpio, GPIO.LOW)
 for lamp in lamps:
-    GPIO.setup(lamp.gpio, GPIO.IN)
+    GPIO.setup(lamp.gpio, GPIO.OUT)
 
-@light_blueprint.route("/status")
+
+def get_lamp_status() -> str:
+    return {lamp.colour:GPIO.input(lamp.gpio) for lamp in lamps}
+
+
+@light_blueprint.route("/lamps/status")
 def status() -> None:
-    return jsonify({lamp.colour:GPIO.input(lamp.gpio) for lamp in lamps})
+    return make_response(jsonify({'status':'OK', 'lamp_state': get_lamp_status()}), 200)
+
+
+@light_blueprint.route("/lamps/set")
+def set_state() -> str:
+    if not request.args.get('lamp'):
+        return make_response(jsonify('Failed, no lamp requested'), 404)
+    if not request.args.get('state'):
+        return make_response(jsonify('Failed, no state requested'), 404)
+    requested_lamp = request.args.get('lamp')
+    requested_state = request.args.get('state')
+    if requested_state == 'on':
+        actuate = GPIO.HIGH
+    elif requested_state == 'off':
+        actuate = GPIO.LOW
+    else:
+        return make_response(jsonify('Failed, no matching state'), 404)
+    if requested_lamp == 'green':
+        GPIO.output(green.gpio, actuate)
+    elif requested_lamp == 'amber':
+        GPIO.output(amber.gpio, actuate)
+    elif requested_lamp == 'red':
+        GPIO.output(red.gpio, actuate)
+    elif requested_lamp == 'all':
+        [GPIO.output(lamp.gpio, actuate) for lamp in lamps]
+    else:
+       return make_response(jsonify('Failed, no matching lamp'), 404)
+    return make_response(jsonify({'status':'OK', 'lamp_state': get_lamp_status()}), 200)
 
